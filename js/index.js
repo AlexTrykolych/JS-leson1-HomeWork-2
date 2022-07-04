7ba447b3d273013771bd8e2cf59f72170c27ea6c
AlexTrykolych <78957084+AlexTrykolych@users.noreply.github.com>
2022-07-04 22:35:09 +0300

    init

---
+++ js/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let name = 'Джон';
+let admin;
+alert(admin = name);
+
+let planetEarth = 'Планета Земля';
+
+let currentUser = 'Текущий пользыватель';
+
+let number = '10' + 10;
+console.log(number);
+console.log(typeof number);
